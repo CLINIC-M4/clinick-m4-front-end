869f099b73a514ab07eadd25bbb90494bc2b61ac
Bruno Azevedo <93809602+brunoacev@users.noreply.github.com>
2022-11-08 15:39:31 -0300

    Inicial Commit

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <h1>Olá!</h1>
+    </div>
+  );
+}
